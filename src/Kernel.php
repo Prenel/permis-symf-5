1580be1c897625ceebffa13518a797ae2a47faaa
PRENEL <thomas.prenel@cgi.com>
2025-01-22 14:59:58 +0100

    test

--- src/Kernel.php
+++ src/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends BaseKernel
     {
         // Charge les fichiers YAML pour les routes classiques
         $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        // $routes->import('../config/{routes}/*.yaml');
 
         // Charge les routes définies par des attributs PHP 8 dans src/Controller/
         $routes->import('../src/Controller/', 'attribute');
